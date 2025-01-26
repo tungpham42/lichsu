641dcc665dc13e82635c40dbf9aa47945c89b54f
Tung Pham <tung.42@gmail.com>
2025-01-26 19:24:57 +0700

    Update WheelOfFortune.js

--- src/components/WheelOfFortune.js
+++ src/components/WheelOfFortune.js
@@ -11,6 +11,10 @@ const wheelItems = [
   { label: "300", value: 300 },
   { label: "400", value: 400 },
   { label: "500", value: 500 },
+  { label: "600", value: 600 },
+  { label: "700", value: 700 },
+  { label: "800", value: 800 },
+  { label: "900", value: 900 },
   { label: "Mất điểm", value: 0 },
   { label: "Mất lượt", value: 0 },
 ];
