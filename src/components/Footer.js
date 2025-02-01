9b3e440b7afeb338b00284dae61a7c86eaa2081f
Tung Pham <tung.42@gmail.com>
2025-02-01 14:38:46 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,17 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-white pt-3 pb-1 mt-5 shadow-lg">
       <Container>
-        <p className="text-center">&copy; {currentYear} Phạm Tùng.</p>
+        <p className="text-center">
+          &copy; {currentYear}{" "}
+          <a
+            className="text-white font-weight-bold text-decoration-none"
+            href="https://tungpham42.github.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Phạm Tùng
+          </a>
+        </p>
       </Container>
     </footer>
   );
