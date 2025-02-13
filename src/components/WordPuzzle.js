02bdc8a452225064a6a69ce4ac078751806118f3
Tung Pham <tung.42@gmail.com>
2025-02-13 07:31:54 +0700

    Update WordPuzzle.js

--- src/components/WordPuzzle.js
+++ src/components/WordPuzzle.js
@@ -291,7 +291,7 @@ const WordPuzzle = () => {
         <>
           <Row className="justify-content-center">
             <Col xs="auto">
-              <h3>Gợi ý: {clue}</h3>
+              <h3 className="text-center">Gợi ý: {clue}</h3>
               <h3 className="text-center display-6 masked-word">
                 {getMaskedWord()}
               </h3>
