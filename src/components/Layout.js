69dd5d8915782d6bc0b1ef7921f89aa07b2fc20e
Tung Pham <tung.42@gmail.com>
2025-02-15 06:43:07 +0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import Header from "./Header";
 import Footer from "./Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Layout = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowButton(window.scrollY > 148);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <Header />
@@ -12,9 +29,26 @@ const Layout = () => {
         className="my-5 pt-0"
         style={{ minHeight: "calc(4px - 10rem + 100vh)" }}
       >
-        <Outlet /> {/* Renders child routes */}
+        <Outlet />
       </Container>
       <Footer />
+
+      {showButton && (
+        <Button
+          variant="primary"
+          onClick={scrollToTop}
+          className="d-flex align-items-center justify-content-center position-fixed bottom-3 end-3 rounded-circle shadow"
+          style={{
+            bottom: "25px",
+            right: "25px",
+            width: "50px",
+            height: "50px",
+            fontSize: "20px",
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowUp} />
+        </Button>
+      )}
     </>
   );
 };
