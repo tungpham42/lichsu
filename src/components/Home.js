37d51da18f5d97dd910e38e6139897de8a01bee4
Tung Pham <tung.42@gmail.com>
2025-02-19 09:58:52 +0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,46 @@
 import React from "react";
 import { Row, Col, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
+const quotes = [
+  {
+    text: "Tôi muốn đi ra nước ngoài xem họ làm thế nào rồi trở về giúp đồng bào mình.",
+    author: "Hồ Chí Minh",
+  },
+  {
+    text: "Nước ta bị mất, nhân dân ta đau khổ, tôi không thể ngồi yên.",
+    author: "Hồ Chí Minh",
+  },
+  { text: "Không có gì quý hơn độc lập, tự do.", author: "Hồ Chí Minh" },
+  {
+    text: "Dân ta phải biết sử ta, cho tường gốc tích nước nhà Việt Nam.",
+    author: "Hồ Chí Minh",
+  },
+  {
+    text: "Các vua Hùng đã có công dựng nước, Bác cháu ta phải cùng nhau giữ lấy nước.",
+    author: "Hồ Chí Minh",
+  },
+  {
+    text: "Lịch sử là của nhân dân, không phải của kẻ mạnh.",
+    author: "Trần Hưng Đạo",
+  },
+  {
+    text: "Đất nước ta không có gì quý hơn là một dân tộc đoàn kết.",
+    author: "Nguyễn Ái Quốc",
+  },
+  {
+    text: "Một dân tộc không biết giữ gìn lịch sử của mình sẽ không có tương lai.",
+    author: "Nguyễn Trãi",
+  },
+  {
+    text: "Lịch sử Việt Nam là một lịch sử của những cuộc đấu tranh anh dũng.",
+    author: "Võ Nguyên Giáp",
+  },
+  {
+    text: "Không có gì vĩ đại hơn khi một dân tộc tự mình làm chủ vận mệnh của mình.",
+    author: "Phan Bội Châu",
+  },
+];
+
 const Home = () => {
   return (
     <>
@@ -10,72 +50,12 @@ const Home = () => {
           <Card className="shadow-lg p-4 bg-white rounded">
             <Card.Body>
               <ListGroup>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Tôi muốn đi ra nước ngoài xem họ làm thế nào rồi trở về
-                    giúp đồng bào mình."
-                  </em>
-                  <span className="h4"> - Hồ Chí Minh</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Nước ta bị mất, nhân dân ta đau khổ, tôi không thể ngồi
-                    yên."
-                  </em>
-                  <span className="h4"> - Hồ Chí Minh</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">"Không có gì quý hơn độc lập, tự do."</em>
-                  <span className="h4"> - Hồ Chí Minh</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Dân ta phải biết sử ta, cho tường gốc tích nước nhà Việt
-                    Nam."
-                  </em>
-                  <span className="h4"> - Hồ Chí Minh</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Các vua Hùng đã có công dựng nước, Bác cháu ta phải cùng
-                    nhau giữ lấy nước."
-                  </em>
-                  <span className="h4"> - Hồ Chí Minh</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Lịch sử là của nhân dân, không phải của kẻ mạnh."
-                  </em>
-                  <span className="h4"> - Trần Hưng Đạo</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Đất nước ta không có gì quý hơn là một dân tộc đoàn kết."
-                  </em>
-                  <span className="h4"> - Nguyễn Ái Quốc</span>
-                </ListGroupItem>
-
-                <ListGroupItem>
-                  <em className="h3">
-                    "Một dân tộc không biết giữ gìn lịch sử của mình sẽ không có
-                    tương lai."
-                  </em>
-                  <span className="h4"> - Nguyễn Trãi</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Lịch sử Việt Nam là một lịch sử của những cuộc đấu tranh
-                    anh dũng."
-                  </em>
-                  <span className="h4"> - Võ Nguyên Giáp</span>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <em className="h3">
-                    "Không có gì vĩ đại hơn khi một dân tộc tự mình làm chủ vận
-                    mệnh của mình."
-                  </em>
-                  <span className="h4"> - Phan Bội Châu</span>
-                </ListGroupItem>
+                {quotes.map((quote, index) => (
+                  <ListGroupItem key={index}>
+                    <em className="h3">"{quote.text}"</em>
+                    <span className="h4"> - {quote.author}</span>
+                  </ListGroupItem>
+                ))}
               </ListGroup>
             </Card.Body>
           </Card>
